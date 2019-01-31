cf5302a5fd6c9314b1a406d35bfec2a699c050a7
visualstudio <visualstudio@230-PC29>
2019-01-31 11:47:44 -0500

    i commited

--- c0742602/js/main.js
+++ c0742602/js/main.js
@@ -1,1 +1,7 @@
-﻿// Your code here!
+﻿
+function myFunction() {
+    var cat = new Date();
+
+
+    document.body.innerHTML = "<h1> The date is " + (cat.getMonth() + 1) + " / " + cat.getDate() + " / " + cat.getFullYear() + " </h1> "
+}
